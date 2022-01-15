456ce424dd1daf23146f0fc91f1594a803cf1067
miltonrodrigues29 <rodriguesmilton21@gmail.com>
2022-01-15 17:03:58 +0530

    -

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 
-import Setup from "./tutorial/2-useEffect/setup/1-useEffect-basics";
-// import Final from "./tutorial/2-useEffect/final/1-useEffect-basics";
+import Setup from "./tutorial/1-useState/setup/2-useState-basics";
 function App() {
   return (
     <div className="container">
--- src/tutorial/1-useState/setup/2-useState-basics.js
+++ src/tutorial/1-useState/setup/2-useState-basics.js
@@ -1,7 +1,29 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const UseStateBasics = () => {
-  return <h2>useState basic example</h2>;
+  // const value = useState(1)[0];
+  // const handler = useState(1)[1];
+  // console.log(value, handler);
+
+  const [text, setText] = useState(1);
+  const handleClick = () => {
+    setText(text + 1);
+  };
+
+  const handleClear = () => {
+    setText(0);
+  };
+  return (
+    <React.Fragment>
+      <h2>{text}</h2>
+      <button type="button" className="btn" onClick={handleClick}>
+        Change Title
+      </button>
+      <button type="button" className="btn" onClick={handleClear}>
+        Clear
+      </button>
+    </React.Fragment>
+  );
 };
 
 export default UseStateBasics;
