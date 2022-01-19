b7c92bff5bb3e42a58eb530bb84ccf3942fe5d75
miltonrodrigues29 <rodriguesmilton21@gmail.com>
2022-01-19 21:01:58 +0530

    use Effect hook

--- src/tutorial/2-useEffect/setup/1-useEffect-basics.js
+++ src/tutorial/2-useEffect/setup/1-useEffect-basics.js
@@ -1,9 +1,29 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 // by default runs after every re-render
 // cleanup function
 // second parameter
 const UseEffectBasics = () => {
-  return <h2>useEffect Basics</h2>;
+
+  
+  const [value,setValue] = useState(0)
+
+
+  const increaseCounter = ()=>
+  {
+    setValue(value+1)
+  }
+
+  useEffect(()=>
+  {
+    console.log("Render Use Effect")
+    document.title = `New Messages(${value})`
+  })
+
+  
+  return <>
+  <h1>{value}</h1>
+  <button type="button" className="btn" onClick={increaseCounter}>Increase</button>
+  </>;
 };
 
 export default UseEffectBasics;
